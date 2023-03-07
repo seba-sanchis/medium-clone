6deb41d74d4fcb8be15ffff8ca021e15c1f0f78a
Sebastian Sanchis <seba.sanchis@gmail.com>
2023-03-07 18:32:54 -0300

    display posts

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,10 +3,10 @@ import { Header } from "./";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className="bg-[#388d80]">
       <Header />
       {children}
-    </>
+    </div>
   );
 };
 
